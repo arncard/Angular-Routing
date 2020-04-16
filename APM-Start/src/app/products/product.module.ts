762c0dbdc82fd38f0fc4aeb7e8887a2b86d5e554
rocio.cardenas <rocio.cardenas@gensolve.com>
2020-04-16 22:15:47 +1000

    child routes 1

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -7,6 +7,8 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { ProductResolver } from './products-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 @NgModule({
   imports: [
@@ -16,13 +18,31 @@ import { ProductResolver } from './products-resolver.service';
       { path: 'products/:id', component: ProductDetailComponent,
         resolve: { resolvedData: ProductResolver } },
       { path: 'products/:id/edit', component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver } }
+        resolve: { resolvedData: ProductResolver },
+        children: [
+          {
+            path: '',
+            redirectTo: 'info',
+            pathMatch: 'full'            
+          },
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
+          }
+        ]  
+      }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
